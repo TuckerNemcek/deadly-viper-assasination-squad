0201ae6be2870fae2d40d476d5b09854b9dc5205
Sean Tansey <sean.d.tansey@gmail.com>
2018-11-21 09:39:53 -0700

    Work pls

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-let viperSean = () => {
-  console.log("Yeah, go Sean!")
-=======
 function branchname (){
   alert(intruder alert!)
->>>>>>> c34914df839819d12d9983b2bfd79e908931273d
 }
